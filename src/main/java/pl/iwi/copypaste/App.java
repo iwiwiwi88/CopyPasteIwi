96ecdb0e1e35beaf22cdebb3ed166854a064d7a5
Iwona Sasak <sasak.iwona.88@gmail.com>
2020-04-30 13:13:02 +0200

    Update App.java

--- src/main/java/pl/iwi/copypaste/App.java
+++ src/main/java/pl/iwi/copypaste/App.java
@@ -1,18 +1,22 @@
 package pl.iwi.copypaste;
 
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class App extends Application {
 
+    private static final double WINDOW_WIDTH = 500;
+    private static final String APP_NAME = "CopyPasteIwi";
+    private static final String FXML_MAIN = "/fxml/main.fxml";
+    private static final String IMAGES_APP_ICON = "/images/iwi_icon.png";
     private Stage stage;
 
     public static void main(String[] args) {
@@ -24,12 +28,12 @@ public class App extends Application {
         this.stage = primaryStage;
 
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/fxml/main.fxml"));
+        loader.setLocation(getClass().getResource(FXML_MAIN));
         Parent root = loader.load();
 
         this.stage.setScene(new Scene(root));
-        this.stage.setTitle("CopyPasteIwi");
-        this.stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/iwi_icon.png")));
+        this.stage.setTitle(APP_NAME);
+        this.stage.getIcons().add(new Image(getClass().getResourceAsStream(IMAGES_APP_ICON)));
         setSize();
         pinWindow(true);
 
@@ -37,10 +41,12 @@ public class App extends Application {
     }
 
     private void setSize() {
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
 
-        this.stage.setHeight(screenBounds.getHeight());
-        this.stage.setX(screenBounds.getWidth() - this.stage.getScene().getWidth());
+        this.stage.setX(visualBounds.getWidth() - WINDOW_WIDTH);
+        this.stage.setY(visualBounds.getMinY());
+        this.stage.setWidth(WINDOW_WIDTH);
+        this.stage.setHeight(visualBounds.getHeight());
         this.stage.setResizable(false);
     }
 
